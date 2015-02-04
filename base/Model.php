9f30ea200d47edab0032dea444be37fa1cc53428
James <jpope@Localmarketinginc.com>
2015-02-04 11:06:57 -0500

    First Official v3.0 commit

--- base/Model.php
+++ base/Model.php
@@ -4,7 +4,7 @@
  * This class simply establishes a connection to the database, generically.
  * Any other generic model functions can go here.
  *
- * @version 0.5
+ * @version 1.0
  *
  * */
 
@@ -13,14 +13,16 @@ class Model
 
     public function __construct()
     {
-        require ZXC;
-        $db_config = array(
-            "HOST" => HOST,
-            "NAME" => NAME,
-            "USER" => USER,
-            "PASS" => PASS
-        );
-        ZXC::INIT($db_config);
+        if (!class_exists("ZXC")) {
+            require ZXC;
+            $db_config = array(
+                "HOST" => HOST,
+                "NAME" => NAME,
+                "USER" => USER,
+                "PASS" => PASS
+            );
+            ZXC::INIT($db_config);
+        }
     }
 
 }
