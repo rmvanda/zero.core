c7da668c976275f4af43991dcebe7ac25012a6bc
James <jpope@Localmarketinginc.com>
2015-03-05 11:25:21 -0500

    Added 'helpers' to the autoloading blacklist - and went ahead and turned it into a pyramid, as discussed

--- base/Application.php
+++ base/Application.php
@@ -1,21 +1,21 @@
 <?php
-	/**
-	 * Application class
-	 *
-	 * @version 3.0.1
-	 */
-	class Application
-	{
+    /**
+     * Application class
+     *
+     * @version 3.0.1
+     */
+    class Application
+    {
 
-		private $aspect;
-		private $endpoint;
-		public $request;
-		private $subdomain = array();
+        private $aspect;
+        private $endpoint;
+        public $request;
+        private $subdomain = array();
 
-		public function __construct()
-		{
-			session_start();
-			//@f:off
+        public function __construct()
+        {
+            session_start();
+            //@f:off
 			ini_set("display_errors", "On");
 			error_reporting(-1 & ~E_NOTICE); 
 			
@@ -33,196 +33,190 @@
 		//				   -> totalExecutionTime(xdebug_time_index()) 
 		//				   -> display();
 		//@f:on
-		//	Console::setRequest($this -> request -> aspect, $this -> request -> endpoint, $this -> request -> args);
-		//	Console::saveAutoLoadLog();
+            //	Console::setRequest($this -> request -> aspect, $this -> request
+            // -> endpoint, $this -> request -> args);
+            //	Console::saveAutoLoadLog();
 
-		}
+        }
 
-		public function modprobe(array $modprobe)
-		{
-			foreach ($modprobe as $module) {
-				define(strtoupper($module), true);
-			}
-			return $this;
-		}
+        public function modprobe(array $modprobe)
+        {
+            foreach ($modprobe as $module) {
+                define(strtoupper($module), true);
+            }
+            return $this;
+        }
 
-		public function parseRequest()
-		{
-			$this -> request = new Request();
-			//print_x($this -> request);
-			return $this;
-		}
+        public function parseRequest()
+        {
+            $this -> request = new Request();
+            //print_x($this -> request);
+            return $this;
+        }
 
-		public function run($aspect, $endpoint, $args)
-		{
-			if (in_array(($aspect = ucfirst($aspect)), get_declared_classes()) && !defined("DEV")) {
-				new Error(403);
-			} else {
-				if (loads($aspect)) {
-					$aspect = new $aspect();
-				} else {
-					$aspect = new Response(strtolower($aspect));
-				}
-				$aspect -> endpoint = $endpoint;
-				$aspect -> {$endpoint}($args);
-			}
-		}
+        public function run($aspect, $endpoint, $args)
+        {
+            if (in_array(($aspect = ucfirst($aspect)), get_declared_classes()) && !defined("DEV")) {
+                new Error(403);
+            } else {
+                if (loads($aspect)) {
+                    $aspect = new $aspect();
+                } else {
+                    $aspect = new Response(strtolower($aspect));
+                }
+                $aspect -> endpoint = $endpoint;
+                $aspect -> {$endpoint}($args);
+            }
+        }
 
-		private function friendlyURLConverter($url)
-		{
-			return lcfirst(str_replace(" ", "", ucwords(str_replace("-", ' ', $url))));
-		}
+        private function friendlyURLConverter($url)
+        {
+            return lcfirst(str_replace(" ", "", ucwords(str_replace("-", ' ', $url))));
+        }
 
-		public function load($filename, $path = null)
-		{
+        public function load($filename, $path = null)
+        {
+            if (loads($filename)) {
+                return true;
+            } else {
+                return false;
+            }
 
-			if (loads($filename)) {
-				return true;
-			} else {
-				return false;
-			}
-			//	$stdout = exec("find " . ROOT_PATH . ' -not -iwholename "*admin*" -type f
-			// -name ' . $filename . ".php");
-			//Console::log() -> autoloader("<pre><p>stdout:<p></pre><pre> For $filename:
-			// $stdout<pre>");
-			//		return (file_exists($stdout) ?
-			//	require $stdout : false);
-		}
+        }
 
-		public function suload($filename)
-		{
+        public function suload($filename)
+        {
+            if (suloads($filename)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
 
-			if (suloads($filename)) {
-				return true;
-			} else {
-				return false;
-			}
-			//$stdout = exec("find " . ROOT_PATH . " -type f -name " . $filename . ".php");
-			//return (file_exists($stdout) ?
-			//require $stdout : false);
-		}
+        public function registerAutoloaders($autoloader = null)
+        {
+            // for Composer + PSR compatability
+            if (file_exists($file = ROOT_PATH . "vendor/autoload.php")) {
+                require $file;
+            }
+            // if you want to add external autoloaders
+            if ($autoloader) {
+                if (is_callable($autoloader)) {
+                    spl_autoload_register($autoloader);
+                } elseif (is_array($autoloader)) {
+                    foreach ($autoloader as $al) {
+                        spl_autoload_register($al);
+                    }
+                }
+            }
+            spl_autoload_register("self::load");
+            /**
+             * Maximally Underwritten Fast As Shit Autoloader Array
+             * MUFASA - !
+             * @version 0.8.2
+             *
 
-		public function registerAutoloaders($autoloader = null)
-		{
-			// for Composer + PSR compatability
-			if (file_exists($file = ROOT_PATH . "vendor/autoload.php")) {
-				require $file;
-			}
-			// if you want to add external autoloaders
-			if ($autoloader) {
-				if (is_callable($autoloader)) {
-					spl_autoload_register($autoloader);
-				} elseif (is_array($autoloader)) {
-					foreach ($autoloader as $al) {
-						spl_autoload_register($al);
-					}
-				}
-			}
-			spl_autoload_register("self::load"); 
-			/**
-			 * Maximally Underwritten Fast As Shit Autoloader Array
-			 * MUFASA - !
-			 * @version 0.8.2
-			 *
+             spl_autoload_register(function($class)
+             {
 
-			spl_autoload_register(function($class)
-			{
+             //  echo $class;
+             if (strpos($class, "\\")) {
+             $namespace = explode("\\", $class);
+             $class = array_pop($namespace);
+             }
+             $stdout = exec("find " . ROOT_PATH . ' -not -iwholename "*admin*"
+             -type f -name ' . $class . ".php");
+             //	Console::log() -> autoloading("Class $class in path: $stdout
+             // <br>");
+             //echo " $stdout !<br>";
+             return (file_exists($stdout) ?
+             require $stdout : false);
+             });
+             */
+            spl_autoload_register("self::errorHandler");
+            return $this;
+        }
 
-				//  echo $class;
-				if (strpos($class, "\\")) {
-					$namespace = explode("\\", $class);
-					$class = array_pop($namespace);
-				}
-				$stdout = exec("find " . ROOT_PATH . ' -not -iwholename "*admin*" -type f -name ' . $class . ".php");
-				//	Console::log() -> autoloading("Class $class in path: $stdout <br>");
-				//echo " $stdout !<br>";
-				return (file_exists($stdout) ?
-				require $stdout : false);
-			});
-*/
-			spl_autoload_register("self::errorHandler");
-			return $this;
-		}
+        public function fetchUtilities($utilities = null)
+        {
 
-		public function fetchUtilities($utilities = null)
-		{
+            require ROOT_PATH . "admin/dev/Console/Console.php";
+            require __DIR__ . "/Extensions.php";
+            require __DIR__ . "/../defaults/Error/Error.php";
+            //loads("Error");
 
-			require ROOT_PATH . "admin/dev/Console/Console.php";
-			require __DIR__ . "/Extensions.php";
-			require __DIR__ . "/../defaults/Error/Error.php";
-			//loads("Error");
+            if ($utilities) {
+                if (file_exists($utilities)) {
+                    require $utilities;
+                } elseif (is_array($utilities)) {
+                    foreach ($utilities as $utility) {
+                        require $utility;
+                    }
+                }
+            }
+            return $this;
+        }
 
-			if ($utilities) {
-				if (file_exists($utilities)) {
-					require $utilities;
-				} elseif (is_array($utilities)) {
-					foreach ($utilities as $utility) {
-						require $utility;
-					}
-				}
-			}
-			return $this;
-		}
+        public function defineConstants(array $key = null)
+        {
+            if (($_SERVER['REMOTE_ADDR'] == "192.168.1.77") || $_SERVER['REMOTE_ADDR'] == "50.199.113.222") {
+                define("DEV", true);
+            }
+            if (!defined("ROOT_PATH")) {
+                define("URL", "http://" . $_SERVER['HTTP_HOST'] . "/");
+                define("ROOT_PATH", "/" . trim($_SERVER['DOCUMENT_ROOT'], "app/frontend/www") . "/");
+                //^fuck FIXME
+            }
+            define("VIEW_PATH", ROOT_PATH . "app/frontend/views/");
 
-		public function defineConstants(array $key = null)
-		{
-			if (($_SERVER['REMOTE_ADDR'] == "192.168.1.77") || $_SERVER['REMOTE_ADDR'] == "50.199.113.222") {
-				define("DEV", true);
-			}
-			if (!defined("ROOT_PATH")) {
-				define("URL", "http://" . $_SERVER['HTTP_HOST'] . "/");
-				define("ROOT_PATH", "/" . trim($_SERVER['DOCUMENT_ROOT'], "app/frontend/www") . "/");
-				//^fuck FIXME
-			}
-			define("VIEW_PATH", ROOT_PATH . "app/frontend/views/");
+            foreach (scandir(ROOT_PATH."app/_configs/") as $ini) {
+                if ($ini != "." && $ini != "..") {
+                    foreach (parse_ini_file(ROOT_PATH."app/_configs/".$ini, false, INI_SCANNER_RAW) as $constant => $value) {
+                        define($constant, ($ini == "paths.ini" ? ROOT_PATH : "") . $value);
+                    }
+                }
+            }
 
-			foreach (scandir(ROOT_PATH."app/_configs/") as $ini) {
-				if ($ini != "." && $ini != "..") {
-					foreach (parse_ini_file(ROOT_PATH."app/_configs/".$ini, false, INI_SCANNER_RAW) as $constant => $value) {
-						define($constant, ($ini == "paths.ini" ? ROOT_PATH : "") . $value);
-					}
-				}
-			}
+            if ($key) {
+                foreach ($key as $k => $v) {
+                    define($k, $v);
+                }
+            }
+            return $this;
+        }
 
-			if ($key) {
-				foreach ($key as $k => $v) {
-					define($k, $v);
-				}
-			}
-			return $this;
-		}
+        // TODO
+        // ACL HOOK?
+        public function finalizeRoute()
+        {
 
-		// TODO
-		// ACL HOOK?
-		public function finalizeRoute()
-		{
+            if ($_SERVER['HTTP_HOST'] != PRIMARY_DOMAIN) {
+                if ($_SERVER['HTTP_HOST'] != ADMIN_DOMAIN) {//||
+                    // $_SERVER['HTTP_HOST'] !=
+                    // MOD_DOMAIN)
+                    header("Location: " . $this -> request -> protocol . "://" . PRIMARY_DOMAIN);
+                    exit();
+                } else {
+                    if (!$this -> request -> access) {// -> granted) {
+                        header("Location: " . $this -> request -> protocol . "://" . PRIMARY_DOMAIN);
+                        exit();
+                    } else {
+                        $this -> suload("Admin");
+                        return new Admin();
+                    }
+                }
+            }
+            return $this;
+        }
 
-			if ($_SERVER['HTTP_HOST'] != PRIMARY_DOMAIN) {
-				if ($_SERVER['HTTP_HOST'] != ADMIN_DOMAIN) {//|| $_SERVER['HTTP_HOST'] !=
-					// MOD_DOMAIN)
-					header("Location: " . $this -> request -> protocol . "://" . PRIMARY_DOMAIN);
-					exit();
-				} else {
-					if (!$this -> request -> access) {// -> granted) {
-						header("Location: " . $this -> request -> protocol . "://" . PRIMARY_DOMAIN);
-						exit();
-					} else {
-						$this -> suload("Admin");
-						return new Admin();
-					}
-				}
-			}
-			return $this;
-		}
+        public function errorHandler($class)
+        {
+            if (defined("DEV")) {
+                xdebug_print_function_stack();
+                Console::log() -> error($class);
+                die("<h1 style='color:red'>Can't load $class</h1>");
+            }
+            new Error(404, $class);
+        }
 
-		public function errorHandler($class)
-		{
-			if (defined("DEV")) {
-				xdebug_print_function_stack();
-				Console::log() -> error($class);
-				die("<h1 style='color:red'>Can't load $class</h1>");
-			}
-			new Error(404, $class);
-		}
-
-	}
+    }
--- base/Extensions.php
+++ base/Extensions.php
@@ -1,106 +1,110 @@
 <?php
-	/**
-	 * Redirect -
-	 * Simple alias for header("Location: /*.../*");
-	 * @param $location - either a local location, assuming "/" - or the URL of the
-	 * website.
-	 * @depends URL
-	 *
-	 * @author James Pope
-	 * @version 0.6
-	 *
-	 */
-	function redirect($to)
-	{
-		if (strpos($to, "http://") === false) {
-			$url = URL;
-		}
-		header("Location: " . $url . trim($to, '/'));
-	}
+    /**
+     * Redirect -
+     * Simple alias for header("Location: /*.../*");
+     * @param $location - either a local location, assuming "/" - or the URL of
+     * the
+     * website.
+     * @depends URL
+     *
+     * @author James Pope
+     * @version 0.6
+     *
+     */
+    function redirect($to)
+    {
+        if (strpos($to, "http://") === false) {
+            $url = URL;
+        }
+        header("Location: " . $url . trim($to, '/'));
+    }
 
-	/**
-	 * function formatDate
-	 *
-	 * @return Human Readable date - in Y-M-D unless the timestamp is from the same
-	 * day, in which case, it returns H:M:S
-	 *
-	 * @var $timestamp = Unix Timestamp
-	 *
-	 * @author James Pope
-	 * @version 0.8
-	 *
-	 */
-	function formatDate($timestamp)
-	{
-		return (date("Y-m-d", $timestamp) == date("Y-m-d", time()) ? date("H:i:s", $timestamp) : date("Y-m-d", $timestamp));
-	}
+    /**
+     * function formatDate
+     *
+     * @return Human Readable date - in Y-M-D unless the timestamp is from the
+     * same
+     * day, in which case, it returns H:M:S
+     *
+     * @var $timestamp = Unix Timestamp
+     *
+     * @author James Pope
+     * @version 0.8
+     *
+     */
+    function formatDate($timestamp)
+    {
+        return (date("Y-m-d", $timestamp) == date("Y-m-d", time()) ? date("H:i:s", $timestamp) : date("Y-m-d", $timestamp));
+    }
 
-	/** */
-	function curlImg($img, $imgPath)
-	{
-		$ch = curl_init($img);
-		$fp = fopen($imgPath, 'wb');
-		curl_setopt($ch, CURLOPT_FILE, $fp);
-		curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_exec($ch);
-		curl_close($ch);
-		fclose($fp);
-	}
+    /** */
+    function curlImg($img, $imgPath)
+    {
+        $ch = curl_init($img);
+        $fp = fopen($imgPath, 'wb');
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_exec($ch);
+        curl_close($ch);
+        fclose($fp);
+    }
 
-	function file_curl_contents($url, $timeout = true)
-	{
-		$ch = curl_init();
+    function file_curl_contents($url, $timeout = true)
+    {
+        $ch = curl_init();
 
-		curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-		if ($timeout) {
-			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-			curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-		}
-		$data = curl_exec($ch);
-		curl_close($ch);
+        curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+        if ($timeout) {
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        }
+        $data = curl_exec($ch);
+        curl_close($ch);
 
-		return $data;
-	}
+        return $data;
+    }
 
-	function loads($filename, $path = null)
-	{
-		$stdout = exec("find " . ROOT_PATH . $path . ' -not -iwholename "*admin*" -type f -name ' . $filename . ".php");
-		Console::log() -> loading($filename, $stdout);
-		return (file_exists($stdout) ?
-		require $stdout : false);
-	}
+    function find($file, $path = null)
+    {
+        $stdout = exec($stdin = "find " . ROOT_PATH . $path . ' -not -iwholename "*admin*" -not -iwholename "*helpers*" -type f -name ' . $file . ".php");
+        return (file_exists($stdout) ? $stdout : false);
+    }
 
-	function suloads($filename, $path = null)
-	{
-		$stdout = exec("find " . ROOT_PATH . $path . '  -iwholename "*admin*" -type f -name ' . $filename . ".php");
-		Console::log() -> suloading($filename, $stdout);
-		return (file_exists($stdout) ?
-		require $stdout : false);
-	}
+    function sufind($file, $path = null)
+    {
+        $stdout = exec("find " . ROOT_PATH . $path . '  -iwholename "*admin*" -type f -name ' . $filename . ".php");
+        return (file_exists($stdout) ? $stdout : false);
+    }
 
-	function find($file, $path = null)
-	{
-		$stdout = exec("find " . ROOT_PATH . $path . ' -not -iwholename "*admin*" -type f -name ' . $filename . ".php");
-		return (file_exists($stdout) ? $stdout : false);
-	}
+    function loads($filename, $path = null)
+    {
+        $stdout = find($filename, $path);
+        //exec("find " . ROOT_PATH . $path . ' -not -iwholename "*admin*" -type f
+        // -name ' . $filename . ".php");
+        Console::log() -> loading($filename, $stdout);
+        return (file_exists($stdout) ?
+        require $stdout : false);
+    }
 
-	function pyramid($load)
-	{
-		//Hmm...
-		$include = find($load);
-		$include ?
-		include $include : "";
-	}
+    function suloads($filename, $path = null)
+    {
+        $stdout = sufind($filename, $path);
+        //exec("find " . ROOT_PATH . $path . '  -iwholename "*admin*"
+        // -type f -name ' . $filename . ".php");
+        Console::log() -> suloading($filename, $stdout);
+        return (file_exists($stdout) ?
+        require $stdout : false);
+    }
 
-	function isAjax()
-	{
-		//@f:off
+    function isAjax()
+    {
+        //@f:off
 	        return 
 	          (
 	             @$_SERVER['HTTP_X_REQUESTED_WITH'] && 	
@@ -109,40 +113,40 @@
 	         ? true : false;
 	    }//@f:on
 
-	/**
-	 * Development Utility functions.
-	 */
-	if (defined("DEV")) {
+    /**
+     * Development Utility functions.
+     */
+    if (defined("DEV")) {
 
-		function print_x($x)
-		{
-			if (is_object($x)) {
-				$obj = $x;
-				unset($x);
-				$x['methods'] = get_class_methods($obj);
-				$x['properties'] = get_object_vars($obj);
-			}
-			echo '<pre>';
-			print_r($x);
-			echo '</pre>';
-		}
+        function print_x($x)
+        {
+            if (is_object($x)) {
+                $obj = $x;
+                unset($x);
+                $x['methods'] = get_class_methods($obj);
+                $x['properties'] = get_object_vars($obj);
+            }
+            echo '<pre>';
+            print_r($x);
+            echo '</pre>';
+        }
 
-		function print_i()
-		{
-			echo "<pre>";
-			echo "Peak Memory Usage:  " . xdebug_peak_memory_usage() . "<br>";
-			echo "Total execution Time: " . xdebug_time_index();
-			echo "</pre>";
-		}
+        function print_i()
+        {
+            echo "<pre>";
+            echo "Peak Memory Usage:  " . xdebug_peak_memory_usage() . "<br>";
+            echo "Total execution Time: " . xdebug_time_index();
+            echo "</pre>";
+        }
 
-	} else {
-		// This prevents Errors
-		function print_x()
-		{
-			//pass;
-		};
-		function print_i()
-		{
-			//pass;
-		};
-	}
+    } else {
+        // This prevents Errors
+        function print_x()
+        {
+            //pass;
+        };
+        function print_i()
+        {
+            //pass;
+        };
+    }
