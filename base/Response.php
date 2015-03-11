530d63de427ccdd69cedfa7ea2e5df19b5fc0135
James <jpope@Localmarketinginc.com>
2015-03-11 11:07:30 -0400

    Reviewing this class as a module-base- don't think I changed anything

--- base/Response.php
+++ base/Response.php
@@ -1,130 +1,132 @@
 <?php
 
-class Response
-{
-
-    public $aspect;
-    public $endpoint, $model, $viewPath, $isAjax;
-
-    public function __construct($standalone = false)
+    class Response
     {
-        if ($standalone) {
-            $this -> aspect = $standalone;
-            $this -> viewPath = VIEW_PATH;
-        }
 
-    }
+        public $aspect;
+        public $endpoint, $model, $viewPath, $isAjax;
 
-    public function defineBaseViewPath()
-    {
-        if (file_exists($vp = ROOT_PATH . "opt/" . get_class($this) . "/views/")) {
-            $this -> viewPath = ROOT_PATH . "opt/";
-        } elseif (file_exists($vp = ROOT_PATH . "app/modules/" . get_class($this) . "/views/")) {
-            $this -> viewPath = $vp;
-        } else {
-            $this -> viewPath = VIEW_PATH;
-        }
-    }
-
-    public function __call($func, $args)
-    {
-        if (file_exists($view = ROOT_PATH . "app/modules/" . $this -> aspect . "/views/" . $this -> endpoint . ".php") || file_exists($view = ROOT_PATH . "opt/plugins/Zero/" . $this -> aspect . "/" . $this -> endpoint . ".php") || file_exists($view = VIEW_PATH . $this -> aspect . "/" . $this -> endpoint . ".php")) {
-            $this -> render($view);
-        } else {
-
-            new Error(404, "$func is not a valid thing");
-        }
-    }
-
-    public function render($view)
-    {
-        if (isAjax()) {
-            $this -> getPage($view);
-        } else {
-            $this -> build($view);
-        }
-    }
-
-    public function __get($prop)
-    {
-        if ($prop == 'model') {
-            if (!$this -> _default_model) {
-                $model_name = get_class($this) . '_Model';
-                $this -> _default_model = new $model_name();
+        public function __construct($standalone = false)
+        {
+            if ($standalone) {
+                $this -> aspect = $standalone;
+                $this -> viewPath = VIEW_PATH;
             }
-            return $this -> _default_model;
-        }
-        $model_name = $prop . '_Model';
-        $this -> {$prop} = new $model_name();
-        //prop__CLASS__;
-        $model_name = $prop . '_Model';
-        $this -> {$prop} = new $model_name();
-        return $this -> {$prop};
-    }
 
-    public function build($view)
-    {
-        $this -> buildHead();
-        $this -> buildHeader();
-        $this -> getPage($view);
-        $this -> buildFooter();
-    }
-
-    public function buildHead()
-    {
-        include $this -> viewPath . "_global/head.php";
-    }
-
-    public function buildHeader()
-    {
-        include $this -> viewPath . "_global/header.php";
-    }
-
-    public function getPage($page)
-    {
-        include $page;
-    }
-
-    public function buildFooter()
-    {
-        include $this -> viewPath . "_global/footer.php";
-    }
-
-    /**
-     * Both of these functions need better logic, anyway - - - -
-     *
-     * For instance, in the case of Console - there needs to be a way to load
-     * "Console.js"
-     * - or some kind of hook, perhaps something like wp's enque_script
-     * we'll get there.
-     */
-    private function getStylesheets()
-    {
-        if (file_exists(WEB_PATH . "assets/css/pg-specific/" . $this -> aspect . ".css")) {
-            echo '<link rel="stylesheet" type="text/css" href="/assets/css/pg-specific/' . $this -> aspect . '.css" />';
-        } else {
-            echo "<!-- " . $this -> aspect . ".css not found, so not loaded. -->";
-        }
-        if (file_exists(WEB_PATH . "assets/css/pg-specific/" . $this -> endpoint . ".css")) {
-            echo '<link rel="stylesheet" type="text/css" href="/assets/css/pg-specific/' . $this -> endpoint . '.css" />';
-        } else {
-            echo "<!-- " . $this -> endpoint . ".css not found, so not loaded. -->";
         }
 
-    }
-
-    private function getScripts()
-    {
-        if (file_exists(WEB_PATH . "assets/js/pg-specific/{$this->aspect}.js")) {
-            echo '<script type="text/javascript" src="/assets/js/pg-specific/' . $this -> endpoint . '.js" ></script>';
-        } else {
-            echo "<!-- " . $this -> endpoint . ".js not found, so not loaded. -->";
+        public function defineBaseViewPath()
+        {
+            if (file_exists($vp = ROOT_PATH . "opt/" . get_class($this) . "/views/")) {
+                $this -> viewPath = ROOT_PATH . "opt/";
+            } elseif (file_exists($vp = ROOT_PATH . "app/modules/" . get_class($this) . "/views/")) {
+                $this -> viewPath = $vp;
+            } else {
+                $this -> viewPath = VIEW_PATH;
+            }
         }
-        if (file_exists(WEB_PATH . "assets/js/pg-specific/{$this->endpoint}.js")) {
-            echo '<script type="text/javascript" src="/assets/js/pg-specific/' . $this -> endpoint . '.js" ></script>';
-        } else {
-            echo "<!-- " . $this -> endpoint . ".js not found, so not loaded. -->";
-        }
-    }
 
-}
+        public function __call($func, $args)
+        {
+            //@f:off
+	        if (file_exists($view = ROOT_PATH . "app/modules/" . $this -> aspect . "/views/" . $this -> endpoint . ".php") 
+	        || file_exists($view = ROOT_PATH . "opt/plugins/Zero/" . $this -> aspect . "/" . $this -> endpoint . ".php") 
+	        || file_exists($view = VIEW_PATH . $this -> aspect . "/" . $this -> endpoint . ".php")) {
+	            $this -> render($view);
+    	    } else { //@f:on
+                new Error(404, "$func is not a valid thing");
+            }
+        }
+
+        public function render($view)
+        {
+            if (isAjax()) {
+                $this -> getPage($view);
+            } else {
+                $this -> build($view);
+            }
+        }
+
+        public function __get($prop)
+        {
+            if ($prop == 'model') {
+                if (!$this -> _default_model) {
+                    $model_name = get_class($this) . '_Model';
+                    $this -> _default_model = new $model_name();
+                }
+                return $this -> _default_model;
+            }
+            $model_name = $prop . '_Model';
+            $this -> {$prop} = new $model_name();
+            //prop__CLASS__;
+            $model_name = $prop . '_Model';
+            $this -> {$prop} = new $model_name();
+            return $this -> {$prop};
+        }
+
+        public function build($view)
+        {
+            $this -> buildHead();
+            $this -> buildHeader();
+            $this -> getPage($view);
+            $this -> buildFooter();
+        }
+
+        public function buildHead()
+        {
+            include $this -> viewPath . "_global/head.php";
+        }
+
+        public function buildHeader()
+        {
+            include $this -> viewPath . "_global/header.php";
+        }
+
+        public function getPage($page)
+        {
+            include $page;
+        }
+
+        public function buildFooter()
+        {
+            include $this -> viewPath . "_global/footer.php";
+        }
+
+        /**
+         * Both of these functions need better logic, anyway - - - -
+         *
+         * For instance, in the case of Console - there needs to be a way to load
+         * "Console.js"
+         * - or some kind of hook, perhaps something like wp's enque_script
+         * we'll get there.
+         */
+        private function getStylesheets()
+        {
+            if (file_exists(WEB_PATH . "assets/css/pg-specific/" . $this -> aspect . ".css")) {
+                echo '<link rel="stylesheet" type="text/css" href="/assets/css/pg-specific/' . $this -> aspect . '.css" />';
+            } else {
+                echo "<!-- " . $this -> aspect . ".css not found, so not loaded. -->";
+            }
+            if (file_exists(WEB_PATH . "assets/css/pg-specific/" . $this -> endpoint . ".css")) {
+                echo '<link rel="stylesheet" type="text/css" href="/assets/css/pg-specific/' . $this -> endpoint . '.css" />';
+            } else {
+                echo "<!-- " . $this -> endpoint . ".css not found, so not loaded. -->";
+            }
+
+        }
+
+        private function getScripts()
+        {
+            if (file_exists(WEB_PATH . "assets/js/pg-specific/{$this->aspect}.js")) {
+                echo '<script type="text/javascript" src="/assets/js/pg-specific/' . $this -> endpoint . '.js" ></script>';
+            } else {
+                echo "<!-- " . $this -> endpoint . ".js not found, so not loaded. -->";
+            }
+            if (file_exists(WEB_PATH . "assets/js/pg-specific/{$this->endpoint}.js")) {
+                echo '<script type="text/javascript" src="/assets/js/pg-specific/' . $this -> endpoint . '.js" ></script>';
+            } else {
+                echo "<!-- " . $this -> endpoint . ".js not found, so not loaded. -->";
+            }
+        }
+
+    }
