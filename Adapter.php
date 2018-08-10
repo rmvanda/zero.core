f17f1aa26382f2d8f77a8d53aec080bbe29090cf
James <jpope@Localmarketinginc.com>
2018-08-10 19:37:21 -0400

    out of sync...

--- Adapter.php
+++ Adapter.php
@@ -85,17 +85,14 @@ class Adapter{
         if(!isset(self::$obj)){
             self::$obj = new static;    
         } 
-
         Console::log("Called Statically..."); 
         if(property_exists(self::$obj, $func)){
             Console::log("Property $func exists...?");
             return self::$obj->{$func}($args); 
         } else {
             Console::log("Property $func does NOT exist...?");
-            Console::log("The value of $func is ".self::$obj->{$func}); 
             return self::$obj->{$func};
         }
-
     }
 
     /**
@@ -105,19 +102,15 @@ class Adapter{
      */
 
     public function __get($attr){
-        Console::log("Attempting to retrieve attribute $attr from memcached:\n\n".($a=Connector::getMemConnection()->get($attr))."\n\n");
-        return unserialize($a); 
+        return unserialize(Connector::getMemConnection()->get($attr)); 
     }
 
     public function __set($attr,$val){
         $this->{$attr} = $val; 
-        Console::log("attempting to set $attr with the value of $val");
-        Connector::getMemConnection()->set($attr,$sd=serialize($val)); 
-        Console::log("After serializing, value appears as: ".$attr); 
+        Connector::getMemConnection()->set($attr,serialize($val)); 
         $map = json_decode(file_get_contents("/home/james/dev/php/zero/modules/poker/memmap.json"));
         $map->{$attr} = $val; 
         file_put_contents("/home/james/dev/php/zero/modules/poker/memmap.json", json_encode($map, JSON_PRETTY_PRINT)); 
-        Console::log("Complete."); 
     }
 
 }
